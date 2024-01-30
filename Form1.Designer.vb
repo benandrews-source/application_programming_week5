dbbd3005d8cc245a877c5c7a55bf6bd6a5a3bc23
andre <andre@LAPTOP-8CEOGDTF>
2024-01-30 15:55:18 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstMembers = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblMembers = New System.Windows.Forms.Label()
+        Me.mnuEdit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuAscend = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.mnuDescend = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.txtCount = New System.Windows.Forms.TextBox()
+        Me.mnuCreate = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuAdd = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuDelete = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lstMembers
+        '
+        Me.lstMembers.FormattingEnabled = True
+        Me.lstMembers.ItemHeight = 16
+        Me.lstMembers.Location = New System.Drawing.Point(231, 123)
+        Me.lstMembers.Name = "lstMembers"
+        Me.lstMembers.Size = New System.Drawing.Size(296, 260)
+        Me.lstMembers.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuEdit, Me.ToolStripMenuItem1})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(46, 24)
+        Me.mnuFile.Text = "File"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.mnuExit.Size = New System.Drawing.Size(224, 26)
+        Me.mnuExit.Text = "E&xit"
+        '
+        'lblMembers
+        '
+        Me.lblMembers.Font = New System.Drawing.Font("Microsoft YaHei", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMembers.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.lblMembers.Location = New System.Drawing.Point(263, 46)
+        Me.lblMembers.Name = "lblMembers"
+        Me.lblMembers.Size = New System.Drawing.Size(218, 65)
+        Me.lblMembers.TabIndex = 3
+        Me.lblMembers.Text = "Members"
+        Me.lblMembers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'mnuEdit
+        '
+        Me.mnuEdit.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuCreate, Me.mnuAdd, Me.mnuDelete})
+        Me.mnuEdit.Name = "mnuEdit"
+        Me.mnuEdit.Size = New System.Drawing.Size(49, 24)
+        Me.mnuEdit.Text = "Edit"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuAscend, Me.ToolStripSeparator1, Me.mnuDescend})
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(50, 24)
+        Me.ToolStripMenuItem1.Text = "Sort"
+        '
+        'mnuAscend
+        '
+        Me.mnuAscend.Name = "mnuAscend"
+        Me.mnuAscend.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.A), System.Windows.Forms.Keys)
+        Me.mnuAscend.Size = New System.Drawing.Size(224, 26)
+        Me.mnuAscend.Text = "&Ascending"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(221, 6)
+        '
+        'mnuDescend
+        '
+        Me.mnuDescend.Name = "mnuDescend"
+        Me.mnuDescend.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.D), System.Windows.Forms.Keys)
+        Me.mnuDescend.Size = New System.Drawing.Size(224, 26)
+        Me.mnuDescend.Text = "&Descending"
+        '
+        'lblCount
+        '
+        Me.lblCount.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCount.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.lblCount.Location = New System.Drawing.Point(258, 401)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(76, 27)
+        Me.lblCount.TabIndex = 4
+        Me.lblCount.Text = "Count"
+        '
+        'txtCount
+        '
+        Me.txtCount.Location = New System.Drawing.Point(340, 406)
+        Me.txtCount.Name = "txtCount"
+        Me.txtCount.ReadOnly = True
+        Me.txtCount.Size = New System.Drawing.Size(100, 22)
+        Me.txtCount.TabIndex = 5
+        '
+        'mnuCreate
+        '
+        Me.mnuCreate.Name = "mnuCreate"
+        Me.mnuCreate.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.C), System.Windows.Forms.Keys)
+        Me.mnuCreate.Size = New System.Drawing.Size(246, 26)
+        Me.mnuCreate.Text = "&Create File"
+        '
+        'mnuAdd
+        '
+        Me.mnuAdd.Name = "mnuAdd"
+        Me.mnuAdd.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.M), System.Windows.Forms.Keys)
+        Me.mnuAdd.Size = New System.Drawing.Size(246, 26)
+        Me.mnuAdd.Text = "Add &Member"
+        '
+        'mnuDelete
+        '
+        Me.mnuDelete.Name = "mnuDelete"
+        Me.mnuDelete.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.T), System.Windows.Forms.Keys)
+        Me.mnuDelete.Size = New System.Drawing.Size(246, 26)
+        Me.mnuDelete.Text = "Dele&te Member"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtCount)
+        Me.Controls.Add(Me.lblCount)
+        Me.Controls.Add(Me.lblMembers)
+        Me.Controls.Add(Me.lstMembers)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmMain"
+        Me.Text = "Missions"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstMembers As ListBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+    Friend WithEvents lblMembers As Label
+    Friend WithEvents mnuEdit As ToolStripMenuItem
+    Friend WithEvents mnuCreate As ToolStripMenuItem
+    Friend WithEvents mnuAdd As ToolStripMenuItem
+    Friend WithEvents mnuDelete As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents mnuAscend As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents mnuDescend As ToolStripMenuItem
+    Friend WithEvents lblCount As Label
+    Friend WithEvents txtCount As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,118 @@
+﻿' Sequential Access and Files Programming Assignment.
+' This program keeps track of a church's missionaries that are serving around the world.
+' Benjamin Andrews.
+
+
+' “I will not use code that was never covered in class or in our textbook. If you do you must be able to explain your code when asked by the professor.
+' Using code outside of the resources provided, it can be flagged and reported as an academic integrity violation if there is any suspicion of copying/cheating.” 
+
+
+Public Class frmMain
+
+    ' Declare variable to hold filename from input
+    Dim strfilename As String
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Get user input, error check, and open file on load
+
+        strfilename = InputBox("Enter file name.") & ".txt"
+
+        If strfilename = ".txt" Then
+            MessageBox.Show("Please enter a file name.")
+            strfilename = InputBox("Enter file name.") & ".txt"
+        End If
+
+        updateinfo()
+
+    End Sub
+
+    ' Sub to add info to list box.
+    Private Sub updateinfo()
+        ' Sub to update file in list box and keep accurate count
+
+        lstMembers.DataSource = IO.File.ReadAllLines(strfilename)
+        lstMembers.SelectedIndex = -1
+
+        Dim strcount As String
+        strcount = lstMembers.Items.Count()
+        txtCount.Text = strcount
+
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        ' Closes the application
+        Me.Close()
+
+    End Sub
+
+    Private Sub mnuCreate_Click(sender As Object, e As EventArgs) Handles mnuCreate.Click
+        ' Opens input box, error checks for no input and creates file.
+        Dim strnewname As String = InputBox("Enter a file name.")
+        strfilename = strnewname & ".txt"
+        If strfilename = ".txt" Then
+            MessageBox.Show("Enter a file name.")
+            strfilename = strnewname & ".txt"
+        End If
+        Dim sw As IO.StreamWriter = IO.File.CreateText(strfilename)
+        sw.Close()
+
+        updateinfo()
+
+    End Sub
+
+    Private Sub mnuAscend_Click(sender As Object, e As EventArgs) Handles mnuAscend.Click
+        ' Sort the file contents by acscending order
+        Dim ascendquery = From item In IO.File.ReadAllLines(strfilename)
+                          Order By item Ascending
+                          Select item
+        lstMembers.DataSource = ascendquery.ToList
+
+    End Sub
+
+    Private Sub mnuDescend_Click(sender As Object, e As EventArgs) Handles mnuDescend.Click
+        ' Sort the file contents by descending order
+        Dim descendquery = From item In IO.File.ReadAllLines(strfilename)
+                           Order By item Descending
+                           Select item
+        lstMembers.DataSource = descendquery.ToList
+    End Sub
+
+    Private Sub mnuDelete_Click(sender As Object, e As EventArgs) Handles mnuDelete.Click
+        ' Deletes selected item from file.
+
+        Dim strdelete As String
+        strdelete = lstMembers.SelectedItem
+
+        Dim deletequery = From item In IO.File.ReadAllLines(strfilename)
+                          Where item <> strdelete
+                          Select item
+
+        IO.File.WriteAllLines(strfilename, deletequery)
+
+        updateinfo()
+
+
+    End Sub
+
+    Private Sub mnuAdd_Click(sender As Object, e As EventArgs) Handles mnuAdd.Click
+
+        Dim strnewname As String
+        strnewname = InputBox("Enter a name.")
+
+        ' Error check
+
+        If strnewname = "" Then
+            MessageBox.Show("Enter a name.")
+            strnewname = InputBox("Enter a name.")
+        End If
+
+        ' Add input to file.
+        Dim sw As IO.StreamWriter
+        sw = IO.File.AppendText(strfilename)
+        sw.WriteLine(strnewname)
+        sw.Close()
+
+
+        updateinfo()
+    End Sub
+End Class
